bf20b97bcac5992501cb54170f629c5261de73ee
abdullaAshraf <abdulla.ashraf@yahoo.com>
2020-03-20 19:22:45 +0200

    test scheduler

--- clock.py
+++ clock.py
@@ -7,7 +7,9 @@ from task import updatePlayers
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
-def fetch_new_posts():
+def update_players():
+    print('This job is run every one minutes.')
+    return
     succeeded = updatePlayers()
     if succeeded:
         logging.info({"message": "done"})
